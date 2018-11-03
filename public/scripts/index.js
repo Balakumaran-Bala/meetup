f726e2de47b3007561918f28afa102428a251644
aadame3311 <antonio.adame01@utrgv.edu>
2018-11-03 18:44:39 -0500

    drop shadow on search, auto complete and search locations.

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -9,56 +9,50 @@ function init() {
   var searchBox = new google.maps.places.SearchBox(input);
   map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
 
+  // update bounds when the map is dragged.
   map.addListener('bounds_changed', function() {
-    searchBox.setBounds(map.getBounds());
+      searchBox.setBounds(map.getBounds());
   });
-
   var markers = [];
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
+
+  //AUTO COMPLETE
   searchBox.addListener('places_changed', function() {
     var places = searchBox.getPlaces();
-
     if (places.length == 0) {
-      return;
+        return;
     }
-
-    // Clear out the old markers.
+    //clear out old marker 
     markers.forEach(function(marker) {
-      marker.setMap(null);
+        marker.setMap(null);
     });
-    markers = [];
-
-    // For each place, get the icon, name and location.
+    // for each place, get the icon, name and location. 
     var bounds = new google.maps.LatLngBounds();
     places.forEach(function(place) {
-      if (!place.geometry) {
-        console.log("Returned place contains no geometry");
-        return;
-      }
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
+        if (!place.geometry) {
+            console.log("Returned place contains no geometry");
+            return;
+        }
+        var icon = {
+            url: place.icon, 
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(25, 25)
+        };
+        markers.push(new google.maps.Marker({
+            map: map,
+            title: place.name,
+            position: place.geometry.location
+        }));
+        if (place.geometry.viewport) {
+            bounds.union(place.geometry.viewport);
+        } else {
+            bounds.extend(place.geometry.location);
+        }
+    
 
-      // Create a marker for each place.
-      markers.push(new google.maps.Marker({
-        map: map,
-        icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      }));
-
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
     });
     map.fitBounds(bounds);
-  });
+    });
+
 }
