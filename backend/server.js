308e920ef65872bc7911937239a7db0bac1b851e
Andy Prevalsky <andyprevalsky@yahoo.com>
2018-11-03 16:39:10 -0500

    fleshed out framework

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,14 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+var lobby = {
+    destination: {
+        x: 0,
+        y: 0
+    },
+    users: {}
+};
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, '/../')))
@@ -13,19 +21,44 @@ app.use(express.static(path.join(__dirname, '/../')))
 io.on('connection', function (socket) {
 
     socket.on('joinLobby', function(data) {
-        
+        lobby[users][socket.id] = {
+            currentLocation: {
+                
+            },
+            eta: "-1", //estimated time of arrival
+            ttl: "-1", //time to leave
+        };
+        socket.emit('joinedLobby');
     });
 
     socket.on('setDestination', function(data) {
-       
+       lobby[destination][x] = data[x];
+       lobby[destination][y] = data[y];
+       socket.emit('destinationSet');
     });
 
     socket.on('updateLocation', function(data) {
+        lobby[users][socket.id][currentLocation][x] = data[x];
+        lobby[users][socket.id][currentLocation][y] = data[y];
+        socket.emit('updatedLocation');
+    });
 
+    socket.on('departed', function(data) { //calculate everyones time to leave
+        var maxTime = 0;
+        Object.keys(lobby[users]).forEach(function(socketId) {
+            var time = GoogleMapsApi(lobby[users][socketId][currentLocation][x], lobby[users][socket][currentLocation][y]);
+            if (time > maxTime) {
+                maxTime = time;
+            }
+            lobby[users][socketId][ttl] = time;
+        });
+        Object.keys(lobby[users]).forEach(function(socketId) {
+            lobby[users][socketId][ttl] = maxTime - lobby[users][socketId][ttl];
+        })
     });
 
     socket.on('disconnect', function(){
-        
+        console.log("disconnected");
     });
 
 });
