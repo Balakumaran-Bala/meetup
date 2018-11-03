77603a98984f04cda56a6431e1dcceebf51d1cb0
aadame3311 <antonio.adame01@utrgv.edu>
2018-11-03 17:22:04 -0500

    ..:

---
+++ public/scripts/index.js
@@ -0,0 +1,27 @@
+function initMap() {
+    // The location of Uluru
+    var uluru = {lat: -25.344, lng: 131.036};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {
+            zoom: 4, 
+            center: uluru,
+            mapTypeId: 'roadmap',
+            disableDefaultUI: true
+        }
+
+
+    );
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
+    });
+
+
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: uluru, map: map});
+  }
