0032208803f1118c30970f79efdcb8f1e65c1b33
aadame3311 <antonio.adame01@utrgv.edu>
2018-11-04 06:04:54 -0600

    'more info' panel slides up on selection

--- public/scripts/UI.js
+++ public/scripts/UI.js
@@ -1,11 +1,37 @@
-function DisplayModal(name, address) {
+var view_toggle = true;
+
+function DisplayHostModal(name, address) {
     $("#mode-prompt").show();
+    $(".card-header-title").text("Going to "+name+", "+address);
 }
 $("#mod-next").on('click', function() {
-    //$("#mode-prompt").show();
+    $("#mode-prompt").hide();
 
 });
 $("#mod-back").on('click', function() {
     $("#mode-prompt").hide();
 
+});
+$("#details-view").on('click', function() {
+    if (view_toggle) {
+        $(this).animate({
+            top: '-=20vh',
+        }), 500;
+        // change text.
+        $("#details-title").text("Time to leave: ");
+        //change arrow.
+        $(".fas").removeClass("fa-angle-up");
+        $(".fas").addClass("fa-angle-down");
+
+        view_toggle = !view_toggle;
+    } else {
+        $(this).animate({
+            top: '+=20vh',
+        }), 500;
+        $("#details-title").text("More Info ");
+        $(".fas").removeClass("fa-angle-down");
+        $(".fas").addClass("fa-angle-up");
+
+        view_toggle = !view_toggle;
+    }
 });
