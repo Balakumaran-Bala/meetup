26beb634a16c4324c505f4b0fbc9ca0195fdaac9
Andy Prevalsky <andyprevalsky@yahoo.com>
2018-11-04 02:58:40 -0600

    structure changes

--- public/scripts/bot.js
+++ public/scripts/bot.js
@@ -1,11 +1,11 @@
 // //House 3 approx 35 min away
 var socket3 = io.connect('http://localhost:8080');
 socket3.emit('joinLobby', {'name': 'Tony'});
-socket3.emit('updateLocation', {'x': 29.5161656, 'y': -95.28864920000001}); // destination {'x': 29.7853358, 'y': -95.49261059999998}
-var destination = {'x': 29.7853358, 'y': -95.49261059999998};
 var tLoc = [29.5161656, -95.28864920000001];
-var xChange = (29.7853358-29.5161656)/60.0;
-var yChange = (-95.49261059999998+95.28864920000001)/60.0;
+socket3.emit('updateLocation', {'x': tLoc[0], 'y': tLoc[1]}); // destination {'x': 29.7853358, 'y': -95.49261059999998}
+var destination = {'x': 29.7853358, 'y': -95.49261059999998};
+var xChange = (destination.x-tLoc[0])/60.0;
+var yChange = (destination.y+tLoc[1])/60.0;
 var moveTony = function() {
     console.log('move');
     socket3.emit('updateLocation', {'x': tLoc[0], 'y': tLoc[1]});
@@ -22,8 +22,8 @@ socket3.on('displayTTL', function(data) {
 
 var socket4 = io.connect('http://localhost:8080');            
 socket4.emit('joinLobby', {'name': 'Bala'});
-socket4.emit('updateLocation', {'x': 29.6393083, 'y': -95.26521969999999});
 var bLoc = [29.6393083, -95.26521969999999];
+socket4.emit('updateLocation', {'x': bLoc[0], 'y': bloc[1]});
 var xChange2 = (destination.x-bLoc.x)/60;
 var yChange2 = (destination.y-bLoc.y)/60;
 var eta = 0;
